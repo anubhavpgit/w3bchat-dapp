ab15dc39776881f358e90ffea14d82860bf1aab1
Anubhab Patnaik <anubhabr50@gmail.com>
2022-07-22 10:26:00 +0530

    UI mods

--- src/Login.svelte
+++ src/Login.svelte
@@ -1,30 +1,30 @@
 <script>
-	import {user} from "./user";
-	let username;
-	let password;
-	function login(){
-		user.auth(username, password, ({err}) => err && alert(err));
-		
-	}
-	function signup(){
-		user.create(username, password, ({err}) => {
-			if(err){
-				alert(err);
-			}
-			else{
-				login();
-			}
-		}); 
-	}
+  import { user } from './user';
 
+  let username;
+  let password;
 
+  function login() {
+    user.auth(username, password, ({ err }) => err && alert(err));
+  }
+
+  function signup() {
+    user.create(username, password, ({ err }) => {
+      if (err) {
+        alert(err);
+      } else {
+        login();
+      }
+    });
+  }
 </script>
 
-<label for="username">Awesome Username</label>
-<input type="text" id="username" name="username" bind:value={username} placeholder="username" minlenght="3" maxlength="16" />
+<label for="username">Username</label>
+<input name="username" bind:value={username} minlength="3" maxlength="16" />
 
 <label for="password">Password</label>
-<input type="password" bind:value={password} name=password>
+<input name="password" bind:value={password} type="password" />
 
 <button class="login" on:click={login}>Login</button>
-<button class="login" on:click={signup}>Sign Up</button>
+<button class="login"  on:click={signup}>Sign Up</button>
+  
